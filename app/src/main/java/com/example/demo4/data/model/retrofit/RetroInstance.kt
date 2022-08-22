2bc8588e5b32cd713394fe1df1bf9cbe0aaede95
Raghav@adda247 <raghav.garg@addda247.com>
2022-08-22 12:21:36 +0530

    data commit

--- app/src/main/java/com/example/demo4/data/model/retrofit/RetroInstance.kt
+++ app/src/main/java/com/example/demo4/data/model/retrofit/RetroInstance.kt
@@ -3,8 +3,11 @@ package com.example.demo4.data.model.retrofit
 import com.example.demo4.baseUrl
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.properties.Delegates
 
 class RetroInstance {
+    var temp by Delegates.notNull<Int>()
+
     companion object {
         fun getInstance(): Retrofit {
                  return Retrofit.Builder()
